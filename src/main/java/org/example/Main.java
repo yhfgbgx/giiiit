7c1866038194af961754ffcdecd3b3d01edc6509
yhfgbgxGit <1435833592@qq.com>
2022-11-24 15:27:27 +0800

    hotfix cmt

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,8 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("first");
-        System.out.println("hot-fix");
         System.out.println("hotfix");
     }
 }
