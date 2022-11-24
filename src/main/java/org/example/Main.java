12b9e883b590ff51b23420b40f9b97c183c79b03
bin <112109440+yhfgbgx@users.noreply.github.com>
2022-11-24 18:44:21 +0800

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,9 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("first");
-        System.out.println("hot-fix");
-        System.out.println("master test");
-        System.out.println("hotfix");
+        System.out.println("github");
     }
 }
